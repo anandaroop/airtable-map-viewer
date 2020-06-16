9f933d2e00424db1a1b5e18504c8cea40ba6a93c
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-06-16 00:56:08 -0400

    Color code toggler as well

--- components/ViewToggle.tsx
+++ components/ViewToggle.tsx
@@ -6,41 +6,48 @@ interface ViewToggleProps {
   viewItem: ViewsItem;
 }
 
-export const ViewToggle: React.FC<ViewToggleProps> = ({ metaRecordId, viewItem }) => {
+export const ViewToggle: React.FC<ViewToggleProps> = ({
+  metaRecordId,
+  viewItem,
+}) => {
   const selectedIds = useStoreState((state) => state.views.selectedIds);
   const isSelected = selectedIds.includes(metaRecordId);
 
   const selectView = useStoreActions((actions) => actions.views.select);
   const deselectView = useStoreActions((actions) => actions.views.deselect);
 
+  const handleToggle = (e: any) => {
+    e.preventDefault();
+    if (isSelected) {
+      deselectView({ metaRecordId });
+    } else {
+      selectView({ metaRecordId });
+    }
+  };
+
   return (
     <>
       <div
+        tabIndex={0}
         className={`viewToggle ${isSelected ? "selected" : ""}`}
-        onClick={(e) => {
-          e.preventDefault();
-          if (isSelected) {
-            deselectView({ metaRecordId });
-          } else {
-            selectView({ metaRecordId });
-          }
-        }}
+        onClick={handleToggle}
+        onKeyDown={(e) => e.key === "Enter" && handleToggle(e)}
       >
         {viewItem.metadata["Full name"]}
       </div>
 
       <style jsx>{`
         .viewToggle {
-          background: ${isSelected ? "pink" : "initial"};
+          background: ${isSelected ? `${viewItem.defaultColor}99` : "initial"};
           border: solid 1px white;
           cursor: pointer;
           // font-weight: ${isSelected ? "bold" : "normal"};
-          padding: 0.25em;
+          padding: 0.5em;
           user-select: none;
         }
 
         .viewToggle:hover {
-          background: ${isSelected ? "pink" : "#eee"};
+          background: ${isSelected ? viewItem.defaultColor : "#eee"};
         }
       `}</style>
     </>
