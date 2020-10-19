eec3acd920386d46d2752843dade4ecbced05d30
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-10-19 00:25:51 -0400

    feat: link to new fridges site

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <nav>
         <a href="evangel">Route planning tool</a>
-        <a href="fridges">Community Fridges</a>
+        <a href="https://community-fridges.vercel.app/">Community Fridges</a>
         {/*
           // BORKED
           <a href="volunteers-and-requests">Volunteers and requests</a>
